d108f48c4d0e82287afef61e6f9d6ce56abe9b13
bhaskar <bhandari222ji@gmail.com>
2024-12-06 22:49:33 +0530

    fix/error

--- src/hooks/useDownload.ts
+++ src/hooks/useDownload.ts
@@ -2,19 +2,23 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 
-export const useDownload = async(videoUrl:string, itag:number)=>{
-   const {isError, isPending, isSuccess, data, mutate} = useMutation({
-    mutationFn:async()=>{
-        try {
-            const res = await axios.post('/api/download', {
-                videoUrl,
-                itag
-            })
-            return res.data
-        } catch (error) {
-            toast.error(error.message||'error while downloading.')
-        }
-    }
-   })
-   return {isError, isPending, isSuccess, data, mutate};
-}
+const useDownloadHook = () => {
+  const { data, isError, isPending, isSuccess, mutate } = useMutation({
+    mutationFn: async ({ videoUrl, itag }: { videoUrl: string; itag: string }) => {
+      try {
+        const res = await axios.post('/api/download', {
+          videoUrl,
+          itag,
+        });
+        return res;
+      } catch (error) {
+        toast.error('Error while downloading.');
+        throw error; // Ensure the error propagates
+      }
+    },
+  });
+
+  return { data, isError, isPending, isSuccess, mutate };
+};
+
+export default useDownloadHook;
